789fa8edbdb95bfaf680af20ee2d8bb0b9e85f28
Fernando Daniele <fernandodaniele1993@gmail.com>
2020-09-30 12:45:58 -0300

    se agrega E2

---
+++ RTOS1_E2_4teclas/inc/main.h
@@ -0,0 +1,22 @@
+/*=============================================================================
+ * Copyright (c) 2020, Martin N. Menendez <menendezmartin81@gmail.com>
+ * All rights reserved.
+ * License: Free
+ * Date: 2020/09/03
+ * Version: v1.1
+ *===========================================================================*/
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/*==================[inclusiones]============================================*/
+#include "FreeRTOSConfig.h"
+#include "FreeRTOS.h"
+#include "sapi.h"
+#include "keys.h"
+
+/*==================[definiciones y macros]==================================*/
+#define MSG_WELCOME "Ejercicio E_2.\r\n"
+#define BAUD_RATE 115200
+#define UART UART_USB
+
+#endif /* _MAIN_H_ */
---
+++ RTOS1_E2_4teclas/out/libs/sys_newlib/src/system.d
@@ -0,0 +1,2 @@
+misProgramas/RTOS1_E2/out/libs/sys_newlib//src/system.o: \
+ libs/sys_newlib//src/system.c
